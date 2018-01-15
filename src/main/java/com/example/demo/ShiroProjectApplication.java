04c90a492504ac700661c6acfcf181a42a44f9dc
wuhao1994 <justdowh@126.com>
2018-01-15 23:08:53 +0800

    shiro

---
+++ src/main/java/com/example/demo/ShiroProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroProjectApplication.class, args);
+	}
+}
