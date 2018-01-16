f5c451f7a245be57f5c0a14082fefc661846fc9d
=wuhao1994 <=justdowh@126.com>
2018-01-16 19:04:36 +0800

    boot

---
+++ src/main/java/com/example/demo/dao/UserInfoDao.groovy
@@ -0,0 +1,11 @@
+package com.example.demo.dao
+
+import com.example.demo.entity.UserInfo
+import org.springframework.data.jpa.repository.JpaRepository
+
+/**
+ * Created by 格讯科技有限公司 on 2018/1/16.
+ */
+interface UserInfoDao extends JpaRepository<UserInfo,Long>{
+    UserInfo findByUsername(String username);
+}
---
+++ src/main/java/com/example/demo/service/UserInfoService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.UserInfo;
+/**
+ * Created by 格讯科技有限公司 on 2018/1/16.
+ */
+public interface UserInfoService {
+    /**通过username查找用户信息;*/
+    public UserInfo findByUsername(String username);
+}
